fa52f18c984d0612627826e9243bf703e6f2215c
kwak1s1h <kwak1s1h@kakao.com>
2022-07-20 16:01:49 +0900

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
 const ws = require('ws');
 const wss = new ws.Server({ port: 3000 });
+
 let idcnt = 0;
+var roomList = new Array();
 
 wss.on('listening', () => {
   console.log(`server opened on port ${wss.options.port}`);
@@ -42,6 +44,48 @@ function GameData(data, socket) {
     // case 'error':
     //   break;
   }
+}
+
+/**
+ * @param {} 
+ */
+function RoomData() {
+  var result = {
+
+  }
+}
+
+class Room {
+  /**
+   * @param {string} name 
+   * @param {Int16Array} id
+   */
+  constructor(name, id) {
+    this.name = name
+    this.hostID = id;
+    this.userList = [];
+    this.roomInfo = {
+      n : this.name,
+      i : this.hostID
+    }
+  }
+  
+  /**
+   * @param {ws.WebSocket} socket 
+   */
+  addMember(socket) {
+    if(this.userList.length >= 2)
+      socket.send(JSON.stringify({
+        l : 'room',
+        t : 'error',
+        v : 'overflow member'
+      }));
+    this.userList.push(socket.id);
+  }
+  rmMember(socket) {
+    
+  }
+
 }
 
 /**
