73937c5616efb00c3bd25320636083397ac711b5
SEH00N <98886309+SEH00N@users.noreply.github.com>
2022-07-31 20:58:25 +0900

    Update DataManager.cs

--- Client/Assets/01. Scripts/Core/DataManager.cs
+++ Client/Assets/01. Scripts/Core/DataManager.cs
@@ -26,6 +26,17 @@ namespace Core
             else ud = JsonConvert.DeserializeObject<UserData>(udJSON);
         }
 
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                for(int i = 1; i < 5; i++)
+                    for(int j = 1; j < 5; j++)
+                        if(!sd.unlockedStage.Contains(i + "-" + j))
+                            sd.unlockedStage.Add(i + "-" + j);
+            }
+        }
+
         private void SaveStageData()
         {
             string JSON = JsonConvert.SerializeObject(sd);
