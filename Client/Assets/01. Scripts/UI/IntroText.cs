a45fc06a8b5c472571830f33761323855a52c953
SEH00N <98886309+SEH00N@users.noreply.github.com>
2022-08-01 03:17:37 +0900

    asset apply

--- Client/Assets/01. Scripts/UI/IntroText.cs
+++ Client/Assets/01. Scripts/UI/IntroText.cs
@@ -6,6 +6,7 @@ public class IntroText : MonoBehaviour
     [SerializeField] float duration;
     private RectTransform rt = null;
 
+    private Sequence seq;
     private void Awake()
     {
         rt = GetComponent<RectTransform>();
@@ -14,9 +15,15 @@ public class IntroText : MonoBehaviour
 
     private void DoIntro()
     {
-        Sequence seq = DOTween.Sequence();
+        seq = DOTween.Sequence();
+
         seq.Append(rt.DOScale(new Vector3(1.05f, 1.05f), duration)).SetEase(Ease.OutCubic);
         seq.Append(rt.DOScale(new Vector3(0.95f, 0.95f), duration)).SetEase(Ease.Linear);
         seq.SetLoops(-1);
+    }
+
+    private void OnDisable()
+    {
+        seq.Kill();
     }
 }
